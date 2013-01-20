18e9e2cf1f70ef55d0390b0467a9ca0d8e39c31c
Strahinja Val Markovic <val@markovic.io>
2013-01-19 20:17:56 -0800

    Adding the astyle shell script
    Running this script will ensure that the C++ code follows our style guide.

---
+++ style_format.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+astyle \
+--style=attach \
+--indent=spaces=2 \
+--indent-switches \
+--indent-col1-comments \
+--indent-preprocessor \
+--max-instatement-indent=80 \
+--break-blocks \
+--pad-oper \
+--pad-paren-in \
+--pad-header \
+--keep-one-line-blocks \
+--convert-tabs \
+--align-pointer=name \
+--align-reference=name \
+--suffix=none \
+--lineend=linux \
+--recursive \
+--exclude=gmock \
+--exclude=testdata \
+--exclude=ycm_core.cpp \
+--exclude=CustomAssert.h \
+--exclude=CustomAssert.cpp \
+"cpp/ycm/*.cpp" \
+"cpp/ycm/*.h"
