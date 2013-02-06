929cf95a58a3676710f232f6e1f457a47965f54a
Zeh Rizzatti <zehrizzatti@gmail.com>
2013-02-06 13:16:49 -0300

    Add install.sh for easily building

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+
+function cmake_install {
+  if [[ `uname -s` == "Darwin" ]]; then
+    homebrew_cmake_install
+  else
+    linux_cmake_install
+  fi
+}
+
+function homebrew_cmake_install {
+  if [[ `which brew &> /dev/null` ]]; then
+    brew install cmake
+  else
+    echo "Go get homebrew, lazy! And retry."
+    exit 1
+  fi
+}
+
+function install {
+  ycm_dir=`pwd`
+  build_dir=`mktemp -d -t ycm_build`
+  pushd $build_dir
+  cmake $ycm_dir/cpp
+  make
+  popd
+}
+
+function linux_cmake_install {
+  echo "Please install 'cmake' using your package manager and retry."
+  exit 1
+}
+
+if [[ ! -z `which cmake &> /dev/null` ]]; then
+  cmake_install
+fi
+install
