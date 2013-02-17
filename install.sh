a3f6987f992b83c8196da59c3ab086b17ec80ac7
Robert D. Blanchet Jr <xgalaxy@gmail.com>
2013-02-17 09:01:54 -0800

    Work around CMake failure to find proper Python.

--- install.sh
+++ install.sh
@@ -21,11 +21,35 @@ function homebrew_cmake_install {
   fi
 }
 
+function python_finder {
+  python_library="-DPYTHON_LIBRARY="
+  python_include="-DPYTHON_INCLUDE_DIR="
+
+  # The CMake 'FindPythonLibs' Module does not work properly.
+  # So we are forced to do its job for it.
+  python_prefix=$(python-config --prefix | sed 's/^[ \t]*//')
+  if [ -f "${python_prefix}/Python" ]; then
+    python_library+="${python_prefix}/Python"
+    python_include+="${python_prefix}/Headers"
+  else
+    which_python=$(python -c 'import platform;print(platform.python_version())' | sed 's/^[ \t]*//')
+    lib_python="${python_prefix}/lib/libpython${which_python}"
+    if [ -f "${lib_python}.a" ]; then
+      python_library+="${lib_python}.a"
+    else
+      python_library+="${lib_python}.dylib"
+    fi
+    python_include+="${python_prefix}/include/${which_python}"
+  fi
+
+  echo "${python_library} ${python_include}"
+}
+
 function install {
   ycm_dir=`pwd`
   build_dir=`mktemp -d -t ycm_build.XXXX`
   pushd $build_dir
-  cmake -G "Unix Makefiles" $1 . $ycm_dir/cpp
+  cmake -G "Unix Makefiles" $(python_finder) $1 . $ycm_dir/cpp
   make ycm_core
   popd
 }
