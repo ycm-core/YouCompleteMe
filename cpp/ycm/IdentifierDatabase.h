ycm-core/YouCompleteMe

commit 2805b0fe856a66ee80e0b7ff8d7bf9577f87daf5

Better thread safety in IdentifierCompleter
Everything that needs access to filetype_map_ has been split into a new class
called IdentifierDatabase. This class is thread-safe. Previously, multiple
threads could access filetype_map_ at the same time, some reading things from
it, others writing to it. WTF was I doing? My best guess is that I introduced
this vile stupidity when I added the second thread to IdentifierCompleter;
previously it was impossible for multiple threads to stomp on filetype_map_
because only one thread ever accessed it. I changed that some time ago and
forgot to protect filetype_map_.

Idiot.

cpp/ycm/IdentifierDatabase.h
@@ -1,0 +1,86 @@
+// Copyright (C) 2013  Strahinja Val Markovic  <val@markovic.io>
+//
+// This file is part of YouCompleteMe.
+//
+// YouCompleteMe is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// YouCompleteMe is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with YouCompleteMe.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef IDENTIFIERDATABASE_H_ZESX3CVR
+#define IDENTIFIERDATABASE_H_ZESX3CVR
+
+#include <boost/utility.hpp>
+#include <boost/unordered_map.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include <vector>
+#include <string>
+
+namespace YouCompleteMe {
+
+class Candidate;
+class Result;
+class CandidateRepository;
+
+
+// This class stores the database of identifiers the identifier completer has
+// seen. It stores them in a data structure that makes it easy to tell which
+// identifier came from which file and what files have which filetypes.
+//
+// The main point of this class is to isolate the parts of the code that need
+// access to this internal data structure so that it's easier to confirm that
+// mutexes are used correctly to protect concurrent access.
+//
+// This class is thread-safe.
+class IdentifierDatabase : boost::noncopyable {
+public:
+  IdentifierDatabase();
+
+  void AddCandidates(
+    const std::vector< std::string > &new_candidates,
+    const std::string &filetype,
+    const std::string &filepath );
+
+  void ClearCandidatesStoredForFile( const std::string &filetype,
+                                     const std::string &filepath );
+
+  void ResultsForQueryAndType( const std::string &query,
+                               const std::string &filetype,
+                               std::vector< Result > &results ) const;
+
+private:
+  std::list< const Candidate * > &GetCandidateList(
+    const std::string &filetype,
+    const std::string &filepath );
+
+  // filepath -> *( *candidate )
+  typedef boost::unordered_map < std::string,
+          boost::shared_ptr< std::list< const Candidate * > > >
+          FilepathToCandidates;
+
+  // filetype -> *( filepath -> *( *candidate ) )
+  typedef boost::unordered_map < std::string,
+          boost::shared_ptr< FilepathToCandidates > > FiletypeMap;
+
+
+  CandidateRepository &candidate_repository_;
+
+  FiletypeMap filetype_map_;
+  mutable boost::mutex filetype_map_mutex_;
+};
+
+} // namespace YouCompleteMe
+
+
+#endif /* end of include guard: IDENTIFIERDATABASE_H_ZESX3CVR */
+

cpp/ycm/IdentifierCompleter.cpp
@@ -17,21 +17,15 @@
 
 #include "IdentifierCompleter.h"
 #include "standard.h"
-#include "CandidateRepository.h"
+
 #include "Candidate.h"
+#include "IdentifierUtils.h"
 #include "Result.h"
 #include "Utils.h"
-#include "IdentifierUtils.h"
 
-#include <boost/unordered_set.hpp>
 #include <boost/bind.hpp>
 #include <boost/make_shared.hpp>
-#include <boost/algorithm/string.hpp>
-#include <boost/algorithm/cxx11/any_of.hpp>
 #include <algorithm>
-
-using boost::algorithm::any_of;
-using boost::algorithm::is_upper;
 
 using boost::packaged_task;
 using boost::unique_future;
@@ -78,16 +72,14 @@ void BufferIdentifiersThreadMain(
 
 
 IdentifierCompleter::IdentifierCompleter()
-  : candidate_repository_( CandidateRepository::Instance() ),
-    threading_enabled_( false ) {
+  : threading_enabled_( false ) {
 }
 
 
 IdentifierCompleter::IdentifierCompleter(
   const std::vector< std::string > &candidates )
-  : candidate_repository_( CandidateRepository::Instance() ),
-    threading_enabled_( false ) {
-  AddCandidatesToDatabase( candidates, "", "" );
+  : threading_enabled_( false ) {
+  identifier_database_.AddCandidates( candidates, "", "" );
 }
 
 
@@ -95,9 +87,8 @@ IdentifierCompleter::IdentifierCompleter(
   const std::vector< std::string > &candidates,
   const std::string &filetype,
   const std::string &filepath )
-  : candidate_repository_( CandidateRepository::Instance() ),
-    threading_enabled_( false ) {
-  AddCandidatesToDatabase( candidates, filetype, filepath );
+  : threading_enabled_( false ) {
+  identifier_database_.AddCandidates( candidates, filetype, filepath );
 }
 
 
@@ -121,18 +112,12 @@ void IdentifierCompleter::EnableThreading() {
 
 
 void IdentifierCompleter::AddCandidatesToDatabase(
-  const std::vector< std::string > &new_candidates,
-  const std::string &filetype,
-  const std::string &filepath ) {
-  std::list< const Candidate *> &candidates =
-    GetCandidateList( filetype, filepath );
-
-  std::vector< const Candidate * > repository_candidates =
-    candidate_repository_.GetCandidatesForStrings( new_candidates );
-
-  candidates.insert( candidates.end(),
-                     repository_candidates.begin(),
-                     repository_candidates.end() );
+    const std::vector< std::string > &new_candidates,
+    const std::string &filetype,
+    const std::string &filepath ) {
+  identifier_database_.AddCandidates( new_candidates,
+                                      filetype,
+                                      filepath );
 }
 
 
@@ -141,16 +126,17 @@ void IdentifierCompleter::AddCandidatesToDatabaseFromBuffer(
   const std::string &filetype,
   const std::string &filepath,
   bool collect_from_comments_and_strings ) {
-  ClearCandidatesStoredForFile( filetype, filepath );
+  identifier_database_.ClearCandidatesStoredForFile( filetype, filepath );
 
   std::string new_contents =
     collect_from_comments_and_strings ?
     buffer_contents :
     RemoveIdentifierFreeText( buffer_contents );
 
-  AddCandidatesToDatabase( ExtractIdentifiersFromText( new_contents ),
-                           filetype,
-                           filepath );
+  identifier_database_.AddCandidates(
+      ExtractIdentifiersFromText( new_contents ),
+      filetype,
+      filepath );
 }
 
 
@@ -186,7 +172,7 @@ std::vector< std::string > IdentifierCompleter::CandidatesForQueryAndType(
   const std::string &query,
   const std::string &filetype ) const {
   std::vector< Result > results;
-  ResultsForQueryAndType( query, filetype, results );
+  identifier_database_.ResultsForQueryAndType( query, filetype, results );
 
   std::vector< std::string > candidates;
   candidates.reserve( results.size() );
@@ -220,70 +206,6 @@ Future< AsyncResults > IdentifierCompleter::CandidatesForQueryAndTypeAsync(
 
   latest_query_task_.Set( task );
   return Future< AsyncResults >( boost::move( future ) );
-}
-
-
-void IdentifierCompleter::ResultsForQueryAndType(
-  const std::string &query,
-  const std::string &filetype,
-  std::vector< Result > &results ) const {
-  FiletypeMap::const_iterator it = filetype_map_.find( filetype );
-
-  if ( it == filetype_map_.end() || query.empty() )
-    return;
-
-  Bitset query_bitset = LetterBitsetFromString( query );
-  bool query_has_uppercase_letters = any_of( query, is_upper() );
-
-  boost::unordered_set< const Candidate * > seen_candidates;
-  seen_candidates.reserve( candidate_repository_.NumStoredCandidates() );
-
-  foreach ( const FilepathToCandidates::value_type & path_and_candidates,
-            *it->second ) {
-    foreach ( const Candidate * candidate, *path_and_candidates.second ) {
-      if ( ContainsKey( seen_candidates, candidate ) )
-        continue;
-      else
-        seen_candidates.insert( candidate );
-
-      if ( !candidate->MatchesQueryBitset( query_bitset ) )
-        continue;
-
-      Result result = candidate->QueryMatchResult(
-                        query, query_has_uppercase_letters );
-
-      if ( result.IsSubsequence() )
-        results.push_back( result );
-    }
-  }
-
-  std::sort( results.begin(), results.end() );
-}
-
-
-void IdentifierCompleter::ClearCandidatesStoredForFile(
-  const std::string &filetype,
-  const std::string &filepath ) {
-  GetCandidateList( filetype, filepath ).clear();
-}
-
-
-std::list< const Candidate * > &IdentifierCompleter::GetCandidateList(
-  const std::string &filetype,
-  const std::string &filepath ) {
-  boost::shared_ptr< FilepathToCandidates > &path_to_candidates =
-    filetype_map_[ filetype ];
-
-  if ( !path_to_candidates )
-    path_to_candidates.reset( new FilepathToCandidates() );
-
-  boost::shared_ptr< std::list< const Candidate * > > &candidates =
-    ( *path_to_candidates )[ filepath ];
-
-  if ( !candidates )
-    candidates.reset( new std::list< const Candidate * >() );
-
-  return *candidates;
 }
 
 

cpp/ycm/IdentifierCompleter.h
@@ -20,6 +20,7 @@
 
 #include "ConcurrentLatestValue.h"
 #include "ConcurrentStack.h"
+#include "IdentifierDatabase.h"
 #include "Future.h"
 
 #include <boost/utility.hpp>
@@ -34,7 +35,6 @@
 namespace YouCompleteMe {
 
 class Candidate;
-class CandidateRepository;
 
 typedef boost::shared_ptr< std::vector< std::string > > AsyncResults;
 
@@ -92,38 +92,14 @@ public:
 
 private:
 
-  void ResultsForQueryAndType( const std::string &query,
-                               const std::string &filetype,
-                               std::vector< Result > &results ) const;
-
-  void ClearCandidatesStoredForFile( const std::string &filetype,
-                                     const std::string &filepath );
-
-  std::list< const Candidate * > &GetCandidateList(
-    const std::string &filetype,
-    const std::string &filepath );
-
   void InitThreads();
-
-
-  // filepath -> *( *candidate )
-  typedef boost::unordered_map < std::string,
-          boost::shared_ptr< std::list< const Candidate * > > >
-          FilepathToCandidates;
-
-  // filetype -> *( filepath -> *( *candidate ) )
-  typedef boost::unordered_map < std::string,
-          boost::shared_ptr< FilepathToCandidates > > FiletypeMap;
-
 
 
   /////////////////////////////
   // PRIVATE MEMBER VARIABLES
   /////////////////////////////
 
-  CandidateRepository &candidate_repository_;
-
-  FiletypeMap filetype_map_;
+  IdentifierDatabase identifier_database_;
 
   bool threading_enabled_;
 

cpp/ycm/IdentifierDatabase.cpp
@@ -1,0 +1,134 @@
+// Copyright (C) 2013  Strahinja Val Markovic  <val@markovic.io>
+//
+// This file is part of YouCompleteMe.
+//
+// YouCompleteMe is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// YouCompleteMe is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with YouCompleteMe.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "IdentifierDatabase.h"
+#include "standard.h"
+
+#include "Candidate.h"
+#include "CandidateRepository.h"
+#include "IdentifierUtils.h"
+#include "Result.h"
+#include "Utils.h"
+
+#include <boost/thread/locks.hpp>
+#include <boost/unordered_set.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/cxx11/any_of.hpp>
+
+using boost::algorithm::any_of;
+using boost::algorithm::is_upper;
+
+
+namespace YouCompleteMe {
+
+IdentifierDatabase::IdentifierDatabase()
+  : candidate_repository_( CandidateRepository::Instance() ) {
+}
+
+
+void IdentifierDatabase::AddCandidates(
+  const std::vector< std::string > &new_candidates,
+  const std::string &filetype,
+  const std::string &filepath ) {
+  boost::lock_guard< boost::mutex > locker( filetype_map_mutex_ );
+  std::list< const Candidate *> &candidates =
+    GetCandidateList( filetype, filepath );
+
+  std::vector< const Candidate * > repository_candidates =
+    candidate_repository_.GetCandidatesForStrings( new_candidates );
+
+  candidates.insert( candidates.end(),
+                     repository_candidates.begin(),
+                     repository_candidates.end() );
+}
+
+
+void IdentifierDatabase::ClearCandidatesStoredForFile(
+  const std::string &filetype,
+  const std::string &filepath ) {
+  boost::lock_guard< boost::mutex > locker( filetype_map_mutex_ );
+  GetCandidateList( filetype, filepath ).clear();
+}
+
+
+void IdentifierDatabase::ResultsForQueryAndType(
+  const std::string &query,
+  const std::string &filetype,
+  std::vector< Result > &results ) const {
+  FiletypeMap::const_iterator it;
+  {
+    boost::lock_guard< boost::mutex > locker( filetype_map_mutex_ );
+    it = filetype_map_.find( filetype );
+
+    if ( it == filetype_map_.end() || query.empty() )
+      return;
+  }
+  Bitset query_bitset = LetterBitsetFromString( query );
+  bool query_has_uppercase_letters = any_of( query, is_upper() );
+
+  boost::unordered_set< const Candidate * > seen_candidates;
+  seen_candidates.reserve( candidate_repository_.NumStoredCandidates() );
+
+  {
+    boost::lock_guard< boost::mutex > locker( filetype_map_mutex_ );
+    foreach ( const FilepathToCandidates::value_type & path_and_candidates,
+              *it->second ) {
+      foreach ( const Candidate * candidate, *path_and_candidates.second ) {
+        if ( ContainsKey( seen_candidates, candidate ) )
+          continue;
+        else
+          seen_candidates.insert( candidate );
+
+        if ( !candidate->MatchesQueryBitset( query_bitset ) )
+          continue;
+
+        Result result = candidate->QueryMatchResult(
+                          query, query_has_uppercase_letters );
+
+        if ( result.IsSubsequence() )
+          results.push_back( result );
+      }
+    }
+  }
+
+  std::sort( results.begin(), results.end() );
+}
+
+
+// WARNING: You need to hold the filetype_map_mutex_ before calling this
+// function and while using the returned list.
+std::list< const Candidate * > &IdentifierDatabase::GetCandidateList(
+  const std::string &filetype,
+  const std::string &filepath ) {
+  boost::shared_ptr< FilepathToCandidates > &path_to_candidates =
+    filetype_map_[ filetype ];
+
+  if ( !path_to_candidates )
+    path_to_candidates.reset( new FilepathToCandidates() );
+
+  boost::shared_ptr< std::list< const Candidate * > > &candidates =
+    ( *path_to_candidates )[ filepath ];
+
+  if ( !candidates )
+    candidates.reset( new std::list< const Candidate * >() );
+
+  return *candidates;
+}
+
+
+
+} // namespace YouCompleteMe

commit 0134064efb7f42f5dd30f464bfb5ebdcdb70f9d1

Fix errors when building on Windows with VS
This errors was introduced in 2805b0fe856a66ee80e0b7ff8d7bf9577f87daf5.

cpp/ycm/IdentifierDatabase.h
@@ -59,13 +59,15 @@ public:
                                std::vector< Result > &results ) const;
 
 private:
-  std::list< const Candidate * > &GetCandidateList(
+  std::list< const Candidate *,
+    std::allocator < const Candidate * > > &GetCandidateList(
     const std::string &filetype,
     const std::string &filepath );
 
   // filepath -> *( *candidate )
   typedef boost::unordered_map < std::string,
-          boost::shared_ptr< std::list< const Candidate * > > >
+          boost::shared_ptr< std::list< const Candidate *,
+          std::allocator < const Candidate * > > > >
           FilepathToCandidates;
 
   // filetype -> *( filepath -> *( *candidate ) )