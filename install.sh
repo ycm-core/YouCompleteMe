8066bdc29b31d42830868e478678907f5f241000
Chiel92 <ctje92@gmail.com>
2013-07-16 17:17:57 +0200

    omnisharp build added to install script

--- install.sh
+++ install.sh
@@ -102,25 +102,29 @@ function linux_cmake_install {
 }
 
 function usage {
-  echo "Usage: $0 [--clang-completer]"
+  echo "Usage: $0 [--clang-completer] [--cs-completer]"
   exit 0
 }
 
-if [[ $# -gt 1 ]]; then
+if [[ $# -gt 2 ]]; then
   usage
 fi
 
-case "$1" in
-  --clang-completer)
-    cmake_args='-DUSE_CLANG_COMPLETER=ON'
-    ;;
-  '')
-    cmake_args=''
-    ;;
-  *)
-    usage
-    ;;
-esac
+cmake_args=''
+omnisharp_completer=false
+for flag in $@; do
+  case "$flag" in
+    --clang-completer)
+      cmake_args='-DUSE_CLANG_COMPLETER=ON'
+      ;;
+    --omnisharp-completer)
+      omnisharp_completer=true
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
 
 if ! command_exists cmake; then
   echo "CMake is required to build YouCompleteMe."
@@ -133,3 +137,20 @@ else
   testrun $cmake_args $EXTRA_CMAKE_ARGS
 fi
 
+if $omnisharp_completer; then
+  buildcommand="msbuild"
+  if ! command_exists msbuild; then
+    buildcommand="xbuild"
+    if ! command_exists xbuild; then
+      echo "msbuild or xbuild is required to build Omnisharp"
+      exit 1
+    fi
+  fi
+
+  ycm_dir=`pwd`
+  build_dir=$ycm_dir"/python/ycm/completers/cs/OmniSharpServer"
+  
+  cd $build_dir
+  $buildcommand
+  cd $ycm_dir
+fi
