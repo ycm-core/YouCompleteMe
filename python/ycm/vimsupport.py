9d06a3e54cdfa35033434c0127e75d127ad562b6
Strahinja Val Markovic <val@markovic.io>
2014-01-10 14:39:52 -0800

    Revert "Only removing our signs in buffer, no all"
    This reverts commit 182848050e412e3b46f09e928ecc23d32ae9cea1.
    
    The reason we are reverting this is because removing signs in a loop causes
    flicker. The only non-flicker approach is to remove all signs in a buffer with
    "sign unplace buffer=<buffer-num>".
    
    So no compatibility with other plugins for us.

--- python/ycm/vimsupport.py
+++ python/ycm/vimsupport.py
@@ -107,11 +107,11 @@ def GetBufferFilepath( buffer_object ):
   return os.path.join( os.getcwd(), str( buffer_object.number ) )
 
 
-def UnplaceSignsInBuffer( buffer_number, signs ):
+# TODO: only unplace our signs, not all signs
+def UnplaceAllSignsInBuffer( buffer_number ):
   if buffer_number < 0:
     return
-  for sign in signs:
-    vim.command( 'sign unplace {0} buffer={1}'.format( sign, buffer_number ) )
+  vim.command( 'sign unplace * buffer={0}'.format( buffer_number ) )
 
 
 def PlaceSign( sign_id, line_num, buffer_num, is_error = True ):
