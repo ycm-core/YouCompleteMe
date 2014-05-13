005524637d3574c987a156ca389b0fda1065fb4a
Strahinja Val Markovic <val@markovic.io>
2014-05-13 16:12:52 -0700

    build/test scripts split into ycmd/ycm versions

--- run_tests.sh
+++ run_tests.sh
@@ -2,46 +2,16 @@
 
 set -e
 
-function usage {
-  echo "Usage: $0 [--no-clang-completer]"
-  exit 0
-}
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-flake8 --select=F,C9 --max-complexity=10 --exclude=.git,cpp \
-   python third_party/ycmd/ycmd
+$SCRIPT_DIR/third_party/ycmd/run_tests.sh "$@"
 
-use_clang_completer=true
-for flag in $@; do
-  case "$flag" in
-    --no-clang-completer)
-      use_clang_completer=false
-      ;;
-    *)
-      usage
-      ;;
-  esac
-done
+flake8 --select=F,C9 --max-complexity=10 $SCRIPT_DIR/python
 
-if [ -n "$USE_CLANG_COMPLETER" ]; then
-  use_clang_completer=$USE_CLANG_COMPLETER
-fi
-
-if $use_clang_completer; then
-  extra_cmake_args="-DUSE_CLANG_COMPLETER=ON -DUSE_DEV_FLAGS=ON"
-else
-  extra_cmake_args="-DUSE_DEV_FLAGS=ON"
-fi
-
-EXTRA_CMAKE_ARGS=$extra_cmake_args YCM_TESTRUN=1 ./install.sh --omnisharp-completer
-
-for directory in third_party/*; do
+for directory in $SCRIPT_DIR/third_party/*; do
   if [ -d "${directory}" ]; then
-    export PYTHONPATH=$PWD/${directory}:$PYTHONPATH
+    export PYTHONPATH=${directory}:$PYTHONPATH
   fi
 done
 
-if $use_clang_completer; then
-  nosetests -v python third_party/ycmd/ycmd
-else
-  nosetests -v --exclude=".*Clang.*" python third_party/ycmd/ycmd
-fi
+nosetests -v $SCRIPT_DIR/python
