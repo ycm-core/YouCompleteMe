6572635c67df24d8f4955d10ffe9bdede2f84bc1
Strahinja Val Markovic <val@markovic.io>
2014-05-14 15:00:59 -0700

    install.sh now removes old ycm libs if present

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-set -e
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 $SCRIPT_DIR/third_party/ycmd/build.sh "$@"
+
+# Remove old YCM libs if present so that YCM can start.
+rm python/*ycm_core.* &> /dev/null
+rm python/*ycm_client_support.* &> /dev/null
+rm python/*clang*.* &> /dev/null
