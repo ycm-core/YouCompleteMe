6b7e7361571bf6b54dc78c46fe76849c64bb59d5
Strahinja Val Markovic <val@markovic.io>
2014-05-15 09:54:41 -0700

    Using -f with rm to get exit 0 if missing file
    As per comments on https://github.com/Valloric/YouCompleteMe/commit/6572635c67df24d8f4955d10ffe9bdede2f84bc1

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $SCRIPT_DIR/third_party/ycmd/build.sh "$@"
 
 # Remove old YCM libs if present so that YCM can start.
-rm python/*ycm_core.* &> /dev/null
-rm python/*ycm_client_support.* &> /dev/null
-rm python/*clang*.* &> /dev/null
+rm -f python/*ycm_core.* &> /dev/null
+rm -f python/*ycm_client_support.* &> /dev/null
+rm -f python/*clang*.* &> /dev/null
