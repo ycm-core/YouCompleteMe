d5f7e9f9231c4584155782925e649ca7746f8df0
Strahinja Val Markovic <val@markovic.io>
2014-05-21 13:54:31 -0700

    run_tests.sh now builds ycmd first
    Needed to run the tests.

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,8 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+$SCRIPT_DIR/third_party/ycmd/build.sh
+
 flake8 --select=F,C9 --max-complexity=10 $SCRIPT_DIR/python
 
 for directory in $SCRIPT_DIR/third_party/*; do
