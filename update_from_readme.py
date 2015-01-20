6c85a53d4dba4668a517be0346b222704d98df02
Strahinja Val Markovic <val@markovic.io>
2015-01-20 13:07:43 -0800

    Update script now adds IDs for headings

--- update_from_readme.py
+++ update_from_readme.py
@@ -4,6 +4,19 @@ from bs4 import BeautifulSoup
 from markdown import markdown
 import fileinput
 
+def ContentToIdValue( content ):
+  return content.lower().replace( ' ', '-' )
+
+
+def AddIdsForHeadings( soup ):
+  def AddIds( headings ):
+    for heading in headings:
+      heading[ 'id' ] = ContentToIdValue( heading.get_text() )
+
+  for i in xrange( 1, 7 ):
+    AddIds( soup.find_all( 'h' + str( i ) ) )
+  return soup
+
 markdown_lines = list( fileinput.input() )
 
 # We delete the first two lines because that's the big YCM heading which we
@@ -17,10 +30,10 @@ with open('index.html', 'r+') as content_file:
 
   new_contents = markdown( unicode( markdown_source, 'utf-8' ),
                            extensions=['fenced_code'] )
-  new_tags = BeautifulSoup( new_contents, 'html5lib' )
+  new_tags = AddIdsForHeadings( BeautifulSoup( new_contents, 'html5lib' ) )
 
-  soup = BeautifulSoup( content, "html5lib" )
-  elem = soup.find( id="markdown-output" )
+  soup = BeautifulSoup( content, 'html5lib' )
+  elem = soup.find( id='markdown-output' )
   elem.clear()
   for new_elem in new_tags.body.contents:
     elem.append( new_elem )
