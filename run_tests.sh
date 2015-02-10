ad7aeeaf1baabb5b2e3b3a3503ea3bcffeb24134
Ben Jackon <puremourning@gmail.com>
2015-02-10 13:06:00 +0000

    Allow user to skip build when running tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,9 +4,28 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-"${SCRIPT_DIR}/third_party/ycmd/build.py"
+function usage {
+  echo "Usage: $0 [--skip-build]"
+  exit 0
+}
 
 flake8 --select=F,C9 --max-complexity=10 "${SCRIPT_DIR}/python"
+skip_build=false
+
+for flag in $@; do
+  case "$flag" in
+    --skip-build)
+      skip_build=true
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
+if ! $skip_build; then
+    "${SCRIPT_DIR}/third_party/ycmd/build.py"
+fi
 
 for directory in "${SCRIPT_DIR}"/third_party/*; do
   if [ -d "${directory}" ]; then
