0174065cc41eb9c8c11a5855520f34d7622e024e
Andrea Cedraro <a.cedraro@gmail.com>
2015-12-20 09:44:09 +0100

    Use miminum compatible vim version instead of HEAD in Travis

--- travis/travis_install.linux.sh
+++ travis/travis_install.linux.sh
@@ -3,20 +3,8 @@
 virtualenv -p python${YCMD_PYTHON_VERSION} ${YCMD_VENV_DIR}
 
 if [[ ${VIMSCRIPT} ]]; then
-  if [[ -d /tmp/vim/.git ]]; then
-    pushd /tmp/vim
-    git fetch
-    if ! git diff --exit-code --quiet ..origin/master; then
-      git reset --hard origin/master
-      git clean -dfx
-      need_build=1
-    fi
-    popd
-  else
-    git clone --depth 1 --single-branch https://github.com/vim/vim /tmp/vim
-    need_build=1
-  fi
-  if [[ -n "${need_build}" ]]; then
+  if [[ ! -d /tmp/vim/.git ]]; then
+    git clone --depth 1 --single-branch --branch v7.3.598 https://github.com/vim/vim /tmp/vim
     pushd /tmp/vim
     ./configure --prefix="/tmp/vim/build" --with-features=huge \
       --with-x --enable-pythoninterp --enable-fail-if-missing
