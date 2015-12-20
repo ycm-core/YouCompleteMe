e680fed13879a39097da99245dab099ce2cf2de8
Andrea Cedraro <a.cedraro@gmail.com>
2015-12-20 10:04:48 +0100

    Use v7.3.599 instead of v7.3.598
    Apparently version 7.3.598 could not be compiled due to a file not being
    updated with a change. 7.3.599 fixes it

--- travis/travis_install.linux.sh
+++ travis/travis_install.linux.sh
@@ -4,7 +4,7 @@ virtualenv -p python${YCMD_PYTHON_VERSION} ${YCMD_VENV_DIR}
 
 if [[ ${VIMSCRIPT} ]]; then
   if [[ ! -d /tmp/vim/.git ]]; then
-    git clone --depth 1 --single-branch --branch v7.3.598 https://github.com/vim/vim /tmp/vim
+    git clone --depth 1 --single-branch --branch v7.3.599 https://github.com/vim/vim /tmp/vim
     pushd /tmp/vim
     ./configure --prefix="/tmp/vim/build" --with-features=huge \
       --with-x --enable-pythoninterp --enable-fail-if-missing
