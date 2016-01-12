a80846e35dd733f094f8ced9daeacba58eeb4316
micbou <contact@micbou.com>
2016-01-12 21:24:23 +0100

    Check Python interpreter pathname
    Rename CheckPythonVersion to IsPythonVersionCorrect.
    In embedders, sys.executable may contain a Vim path instead of a Python
    one. To avoid starting a Vim instance in this case, we check that given
    path ends with a Python 2.6 or 2.7 name using a regex.
    Add tests for this regex.

---
+++ python/ycm/tests/paths_test.py
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2016 YouCompleteMe contributors
+#
+# This file is part of YouCompleteMe.
+#
+# YouCompleteMe is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# YouCompleteMe is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with YouCompleteMe.  If not, see <http://www.gnu.org/licenses/>.
+
+from ycm.test_utils import MockVimModule
+MockVimModule()
+
+from nose.tools import ok_
+from ycm.paths import EndsWithPython
+
+
+def EndsWithPython_PythonPaths_test():
+  python_paths = [
+    'python',
+    '/usr/bin/python2.6',
+    '/home/user/.pyenv/shims/python2.7',
+    r'C:\aPython26\python.exe'
+  ]
+
+  for path in python_paths:
+    ok_( EndsWithPython( path ) )
+
+
+def EndsWithPython_NotPythonPaths_test():
+  not_python_paths = [
+    '/opt/local/bin/vim',
+    r'C:\Program Files\Vim\vim74\gvim.exe'
+  ]
+
+  for path in not_python_paths:
+    ok_( not EndsWithPython( path ) )
