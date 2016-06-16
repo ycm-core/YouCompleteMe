2b27dd7903196d1aa96733d20256197eddcd5f0c
micbou <contact@micbou.com>
2016-06-16 10:55:32 +0200

    Check Python version in install script
    Add future imports for Python backward compatibility.

--- install.py
+++ install.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
 import os
 import subprocess
 import sys
 import os.path as p
 import glob
+
+PY_MAJOR, PY_MINOR = sys.version_info[ 0 : 2 ]
+if not ( ( PY_MAJOR == 2 and PY_MINOR >= 6 ) or
+         ( PY_MAJOR == 3 and PY_MINOR >= 3 ) or
+         PY_MAJOR > 3 ):
+  sys.exit( 'YouCompleteMe requires Python >= 2.6 or >= 3.3; '
+            'your version of Python is ' + sys.version )
 
 DIR_OF_THIS_SCRIPT = p.dirname( p.abspath( __file__ ) )
 DIR_OF_OLD_LIBS = p.join( DIR_OF_THIS_SCRIPT, 'python' )
