472d2f65452601b978ec7202e8e420f7a4b25db7
micbou <contact@micbou.com>
2016-07-16 17:02:50 +0200

    Initialize all YouCompleteMe members

--- python/ycm/youcompleteme.py
+++ python/ycm/youcompleteme.py
@@ -108,7 +108,9 @@ DIAGNOSTIC_UI_FILETYPES = set( [ 'cpp', 'cs', 'c', 'objc', 'objcpp' ] )
 
 class YouCompleteMe( object ):
   def __init__( self, user_options ):
+    self._available_completers = {}
     self._user_options = user_options
+    self._user_notified_about_crash = False
     self._diag_interface = DiagnosticInterface( user_options )
     self._omnicomp = OmniCompleter( user_options )
     self._latest_file_parse_request = None
