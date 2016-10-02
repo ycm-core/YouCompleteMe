1e8be57708006b88d389e0cf8d887974dfb75992
Ben Jackson <puremourning@gmail.com>
2016-10-02 19:24:45 +0100

    Use g++ 4.8 on YouCompleteMe builds too as the new boost requires this

--- ci/travis/travis_install.linux.sh
+++ ci/travis/travis_install.linux.sh
@@ -1,5 +1,15 @@
 # Linux-specific installation
 
+# We can't use sudo, so we have to approximate the behaviour of setting the
+# default system compiler.
+
+mkdir ${HOME}/bin
+
+ln -s /usr/bin/g++-4.8 ${HOME}/bin/c++
+ln -s /usr/bin/gcc-4.8 ${HOME}/bin/cc
+
+export PATH=${HOME}/bin:${PATH}
+
 # In order to work with ycmd, python *must* be built as a shared library. This
 # is set via the PYTHON_CONFIGURE_OPTS option.
 export PYTHON_CONFIGURE_OPTS="--enable-shared"
