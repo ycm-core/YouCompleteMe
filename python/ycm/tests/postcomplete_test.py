f3ccafe3aedee0dc2ec320e3121869785e730bb8
micbou <contact@micbou.com>
2017-03-09 15:57:27 +0100

    Do not install aliases from future
    Installing aliases from python-future is unreliable and slow.

--- python/ycm/tests/postcomplete_test.py
+++ python/ycm/tests/postcomplete_test.py
@@ -21,8 +21,7 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
-from future import standard_library
-standard_library.install_aliases()
+# Not installing aliases from python-future; it's unreliable and slow.
 from builtins import *  # noqa
 
 from ycm.tests.test_utils import MockVimModule
