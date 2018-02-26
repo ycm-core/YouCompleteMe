ddd5045b18e765c14cc69cb226c0343bf4041186
micbou <contact@micbou.com>
2018-02-26 14:18:30 +0100

    Lower minimum Python version requirements on Travis and CircleCI

--- .circleci/install_dependencies.sh
+++ .circleci/install_dependencies.sh
@@ -47,11 +47,11 @@ PATH="${PYENV_ROOT}/bin:${PATH}"
 eval "$(pyenv init -)"
 
 if [ "${YCMD_PYTHON_VERSION}" == "2.7" ]; then
-  # We need a recent enough version of Python 2.7 on macOS or an error occurs
-  # when installing the psutil dependency for our tests.
-  PYENV_VERSION="2.7.14"
+  # Prior versions fail to compile with error "ld: library not found for
+  # -lSystemStubs"
+  PYENV_VERSION="2.7.2"
 else
-  PYENV_VERSION="3.4.7"
+  PYENV_VERSION="3.4.0"
 fi
 
 # In order to work with ycmd, python *must* be built as a shared library. The
