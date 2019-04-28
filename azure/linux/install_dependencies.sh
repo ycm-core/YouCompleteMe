a2a58ede8846d30dad7a4b1a5898d48bc959f66a
micbou <contact@micbou.com>
2019-04-28 20:25:22 +0200

    Set up CI with Azure Pipelines

---
+++ azure/linux/install_dependencies.sh
@@ -0,0 +1,22 @@
+# Exit immediately if a command returns a non-zero status.
+set -e
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+brew install pyenv
+
+eval "$(pyenv init -)"
+
+# In order to work with ycmd, python *must* be built as a shared library. This
+# is set via the PYTHON_CONFIGURE_OPTS option.
+PYTHON_CONFIGURE_OPTS="--enable-shared" \
+CFLAGS="-I$(brew --prefix openssl)/include" \
+LDFLAGS="-L$(brew --prefix openssl)/lib" \
+pyenv install ${YCM_PYTHON_VERSION}
+pyenv global ${YCM_PYTHON_VERSION}
+
+pip install -r python/test_requirements.txt
+
+set +e
