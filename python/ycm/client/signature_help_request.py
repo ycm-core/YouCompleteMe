26072632293ad370d01e64c9eb738b98e23e0d81
Ben Jackson <puremourning@gmail.com>
2019-05-28 23:15:24 +0100

    Support for signature help
    This uses Vim's new popup windows (where available). When the functions
    we require are not available, we simply don't send the request and thus
    never attempt to show any signatures.
    
    The biggest challenge is placement of the popup, which is essentially
    manual for us. Note that the popup position passed to Vim must be the
    _screen_ position, not a buffer position. So we attempt to mimic
    the way text properties follow text, by anchoring the popup to a
    specific character in a specific buffer. We use the 'screenpos' function
    for this.
    
    The idea is to place the popup:
    
    * imemdiately above the triggering character (named the 'anchor'), not
      overlapping the cursor if there is room above for all the items.
    * otherwise, immediately below the triggering character.
    
    Complications include:
    
    * Ensuring there is room. If there is no room, just don't display.
    * Avoiding overlapping the PUM. This is tricky as the pum is displayed
      asynchronously. We use the CompleteChanged event to try and recaculate
      our popup position when it moves. Again, if we can't display without
      overlapping, just don't display.
    
    The rationale for not displaying is that signature help is secondary
    information that may also be abailabel from the preview window (or
    preview popup), so should not interfere with the primary completion
    mechanism.

---
+++ python/ycm/client/signature_help_request.py
@@ -0,0 +1,106 @@
+# Copyright (C) 2019 YouCompleteMe contributors
+#
+# This file is part of YouCompleteMe.
+#
+# YouCompleteMe is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# YouCompleteMe is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with YouCompleteMe.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+
+import logging
+from ycm.client.base_request import ( BaseRequest, DisplayServerException,
+                                      MakeServerException )
+
+_logger = logging.getLogger( __name__ )
+
+
+class SigHelpAvailableByFileType( dict ):
+  def __missing__( self, filetype ):
+    request = SignatureHelpAvailableRequest( filetype )
+    self[ filetype ] = request
+    return request
+
+
+class SignatureHelpRequest( BaseRequest ):
+  def __init__( self, request_data ):
+    super( SignatureHelpRequest, self ).__init__()
+    self.request_data = request_data
+    self._response_future = None
+
+
+  def Start( self ):
+    self._response_future = self.PostDataToHandlerAsync( self.request_data,
+                                                         'signature_help' )
+
+
+  def Done( self ):
+    return bool( self._response_future ) and self._response_future.done()
+
+
+  def Reset( self ):
+    self._response_future = None
+
+
+  def Response( self ):
+    if not self._response_future:
+      return {}
+
+    response = self.HandleFuture( self._response_future,
+                                  truncate_message = True )
+    if not response:
+      return {}
+
+    # Vim may not be able to convert the 'errors' entry to its internal format
+    # so we remove it from the response.
+    errors = response.pop( 'errors', [] )
+    for e in errors:
+      exception = MakeServerException( e )
+      _logger.error( exception )
+      DisplayServerException( exception, truncate_message = True )
+
+    return response.get( 'signature_help' ) or {}
+
+
+class SignatureHelpAvailableRequest( BaseRequest ):
+  def __init__( self, filetype ):
+    super( SignatureHelpAvailableRequest, self ).__init__()
+    self._response_future = None
+    self.Start( filetype )
+
+
+  def Done( self ):
+    return bool( self._response_future ) and self._response_future.done()
+
+
+  def Response( self ):
+    if not self._response_future:
+      return None
+
+    response = self.HandleFuture( self._response_future,
+                                  truncate_message = True )
+
+    if not response:
+      return None
+
+    return response[ 'available' ]
+
+
+  def Start( self, filetype ):
+    self._response_future = self.GetDataFromHandlerAsync(
+      'signature_help_available',
+      payload = { 'subserver': filetype } )
