8a61ac395c04cee5536991cad4eb56b9efa5fc3d
Ben Jackson <puremourning@gmail.com>
2019-07-14 19:22:05 +0100

    Azure: vim tests

---
+++ azure/linux/build_vim.sh
@@ -0,0 +1,26 @@
+# Exit immediately if a command returns a non-zero status.
+set -e
+
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+pyenv global ${YCM_PYTHON_VERSION}
+
+if [ -d ~/vim ]; then
+  rm -rf ~/vim
+fi
+
+git clone --depth=1 https://github.com/vim/vim
+pushd vim
+  ./configure --with-features=huge \
+              --enable-terminal \
+              --enable-multibyte \
+              --enable-fail-if-missing \
+              ${YCM_VIM_PYTHON}
+  make -j 4
+  make install
+popd
+
+# Also build ycmd
+python install.py --clangd-completer
+
+set +e
