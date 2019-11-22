a76d89a645ed9d328af9eab899fcc0e1177b32a3
Boris Staletic <boris.staletic@gmail.com>
2019-11-22 17:33:40 +0100

    Use openssl 1.1 and newer python

--- azure/linux/install_dependencies.sh
+++ azure/linux/install_dependencies.sh
@@ -5,15 +5,15 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/i
 
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
-brew install openssl@1.0 pyenv
+brew install pyenv
 
 eval "$(pyenv init -)"
 
 # In order to work with ycmd, python *must* be built as a shared library. This
 # is set via the PYTHON_CONFIGURE_OPTS option.
 PYTHON_CONFIGURE_OPTS="--enable-shared" \
-CFLAGS="-I$(brew --prefix openssl@1.0)/include" \
-LDFLAGS="-L$(brew --prefix openssl@1.0)/lib" \
+CFLAGS="-I$(brew --prefix openssl)/include" \
+LDFLAGS="-L$(brew --prefix openssl)/lib" \
   pyenv install ${YCM_PYTHON_VERSION}
 pyenv global ${YCM_PYTHON_VERSION}
 pip install -r python/test_requirements.txt
