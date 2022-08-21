7df663121f7c120072c7aba42aaf3a976a62aeb3
Ben Jackson <puremourning@gmail.com>
2022-08-21 13:14:13 +0100

    Ensure pyright also works

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -29,44 +29,14 @@
 # For more information, please refer to <http://unlicense.org/>
 
 import os.path as p
-import subprocess
 
 DIR_OF_THIS_SCRIPT = p.abspath( p.dirname( __file__ ) )
 DIR_OF_THIRD_PARTY = p.join( DIR_OF_THIS_SCRIPT, 'third_party' )
 
 
-def GetStandardLibraryIndexInSysPath( sys_path ):
-  for index, path in enumerate( sys_path ):
-    if p.isfile( p.join( path, 'os.py' ) ):
-      return index
-  raise RuntimeError( 'Could not find standard library path in Python path.' )
-
-
 def PythonSysPath( **kwargs ):
   sys_path = kwargs[ 'sys_path' ]
-
   dependencies = [ p.join( DIR_OF_THIS_SCRIPT, 'python' ),
-                   p.join( DIR_OF_THIRD_PARTY, 'requests-futures' ),
-                   p.join( DIR_OF_THIRD_PARTY, 'ycmd' ),
-                   p.join( DIR_OF_THIRD_PARTY, 'requests_deps', 'idna' ),
-                   p.join( DIR_OF_THIRD_PARTY, 'requests_deps', 'chardet' ),
-                   p.join( DIR_OF_THIRD_PARTY,
-                           'requests_deps',
-                           'urllib3',
-                           'src' ),
-                   p.join( DIR_OF_THIRD_PARTY, 'requests_deps', 'certifi' ),
-                   p.join( DIR_OF_THIRD_PARTY, 'requests_deps', 'requests' ) ]
-
-  # The concurrent.futures module is part of the standard library on Python 3.
-  interpreter_path = kwargs[ 'interpreter_path' ]
-  major_version = int( subprocess.check_output( [
-    interpreter_path, '-c', 'import sys; print( sys.version_info[ 0 ] )' ]
-  ).rstrip().decode( 'utf8' ) )
-  if major_version == 2:
-    dependencies.append( p.join( DIR_OF_THIRD_PARTY, 'pythonfutures' ) )
-
+                   p.join( DIR_OF_THIRD_PARTY, 'ycmd' ) ]
   sys_path[ 0:0 ] = dependencies
-  sys_path.insert( GetStandardLibraryIndexInSysPath( sys_path ) + 1,
-                   p.join( DIR_OF_THIRD_PARTY, 'python-future', 'src' ) )
-
   return sys_path
