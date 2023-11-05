7a8794457c9aa60ca99b8336bccaa7ee5eb6ab1f
Boris Staletic <boris.staletic@protonmail.com>
2023-11-05 22:33:54 +0100

    Store multiline diags for each line they appear on
    Diagnostic interface used to store multiline diags only on thte starting
    line.
    With this pull commit, _line_to_diags stores a multiline diagnostic for
    each line in the range `diag['range']['start'/'end'].

--- python/ycm/tests/youcompleteme_test.py
+++ python/ycm/tests/youcompleteme_test.py
@@ -78,15 +78,14 @@ def YouCompleteMe_UpdateDiagnosticInterface( ycm, post_vim_message, *args ):
       'line_num': 2,
       'column_num': 31
     },
-    # Looks strange but this is really what ycmd is returning.
     'location_extent': {
       'start': {
-        'filepath': '',
+        'filepath': 'buffer',
         'line_num': 2,
         'column_num': 31,
       },
       'end': {
-        'filepath': '',
+        'filepath': 'buffer',
         'line_num': 2,
         'column_num': 32,
       }
@@ -624,6 +623,18 @@ class YouCompleteMeTest( TestCase ):
         'filepath': 'buffer',
         'line_num': 19,
         'column_num': 2
+      },
+      'location_extent': {
+        'start': {
+          'filepath': 'buffer',
+          'line_num': 19,
+          'column_num': 2
+        },
+        'end': {
+          'filepath': 'buffer',
+          'line_num': 19,
+          'column_num': 3
+        }
       }
     }
 
@@ -673,6 +684,18 @@ class YouCompleteMeTest( TestCase ):
         'filepath': 'buffer',
         'line_num': 19,
         'column_num': 2
+      },
+      'location_extent': {
+        'start': {
+          'filepath': 'buffer',
+          'line_num': 19,
+          'column_num': 2
+        },
+        'end': {
+          'filepath': 'buffer',
+          'line_num': 19,
+          'column_num': 2
+        }
       }
     }
 
